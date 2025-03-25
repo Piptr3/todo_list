ead6ac5de6be951f547114953d13f275753f675e
Piptr3 <piptr24@gmail.com>
2025-03-25 19:58:07 +0100

    implement footer and header skeleton

--- src/modules/UImanager.js
+++ src/modules/UImanager.js
@@ -1,14 +1,37 @@
 export function loadUI() {
-    const header = document.getElementById("header");
+    loadHeader()
     loadMain();
-    const footer = document.getElementById("footer");
+    loadFooter()
+    
+}
+
+function loadHeader() {
+    const header = document.getElementById("header");
+
+    const logo = document.createElement("div");
+    logo.classList.add("logo");
+    logo.textContent = "Todo List";
+
+    header.appendChild(logo);
+
 }
 
 function loadMain() {
     const main = document.getElementById("main");
+
     const container = document.createElement("div");
     container.classList.add("main-container");
     container.textContent = "temp text";
 
     main.appendChild(container);
+}
+
+function loadFooter() {
+    const footer = document.getElementById("footer");
+
+    const copyright = document.createElement("div");
+    copyright.classList.add("copyright");
+    copyright.textContent = "Copyright @ 2025 Piptr3" 
+
+    footer.appendChild(copyright);
 }
